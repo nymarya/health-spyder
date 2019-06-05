3819ed31e7f8b92aab2248df870add19f59d052c
Mayra Azevedo <mayradazevedo@ufrn.edu.br>
2019-06-05 15:11:19 -0300

    build dataframe

---
+++ src/main.py
@@ -0,0 +1,5 @@
+import scrapper
+
+# Recover all tables
+scrapper.recover(240010)
+# Save them into a csv file
--- src/scrapper.py
+++ src/scrapper.py
@@ -1,39 +1,63 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-    'User-Agent': 'google-colab',
-    'Accept': 'text/html, */*',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Connection': 'keep-alive',
-}
 
-data = {
-    'MIME Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'id2': 240010
-}
+def recover(city):
+    """ Recover cities by IBGE code
+        -- city: IBGE code
+    """
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'User-Agent': 'google-colab',
+        'Accept': 'text/html, */*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+    }
 
-response = requests.post("http://indicadoressifilis.aids.gov.br/tabelas.php",
-                       headers=headers, data={})
+    data = {
+        'MIME Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'id2': city
+    }
 
-html_data = response.text
-print(type(html_data))
+    url = "http://indicadoressifilis.aids.gov.br/tabelas.php"
+    response = requests.post(url, headers=headers, data={})
 
-# Create a BeautifulSoup object from the HTML: soup
-soup = BeautifulSoup(html_data, features="html.parser")
+    html_data = response.text
+    print(type(html_data))
 
-# # Prettify the BeautifulSoup object: pretty_soup
-# pretty_soup = soup.prettify()
+    # Create a BeautifulSoup object from the HTML: soup
+    soup = BeautifulSoup(html_data, features="html.parser")
 
-# # Print the response
-# print(pretty_soup)
+    print('Searching for table 10.A')
 
-print('Searching for table 10.A')
-# Search for Tabela 10.A - Casos de sífilis congênita segundo diagnóstico final por ano de diagnóstico. Brasil, 1998-2018.
-tables = soup.findAll("table")
-for table in tables:
-    table_label = table.previous_element
-    if "Tabela 10.A" in table_label:
-        print(table.prettify())
+    # Search for Tabela 10.A - Casos de sífilis congênita segundo diagnóstico
+    # final por ano de diagnóstico. Brasil, 1998-2018.
+    tables = soup.findAll("table")
+    for table in tables:
+        table_label = table.previous_element
+        if "Tabela 10.A" in table_label:
+            # Find all columns that describe the years
+            tag = "headertab"
+            columns_year = [c.text for c in table.find(id=tag).findAll('th')]
+            columns_year.pop(0)
+
+            df = pd.read_html(str(table))[0]
+            df['ibge'] = city
+
+            final_df = df.pivot(index='ibge', columns='Diagnóstico Final',
+                                values=columns_year)
+
+            return final_df
+
+    return None
+
+
+def recover_all():
+    """ Recover all cities
+    """
+
+
+
+
