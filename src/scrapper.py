cd48c45314e98c01c29eced3c22b7de341327438
Mayra Azevedo <mayradazevedo@gmail.com>
2020-02-08 12:44:18 -0300

    update gitignore

--- src/scrapper.py
+++ src/scrapper.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+import multiprocessing as mp
+
 
 def recover(city):
     """ Recover cities by IBGE code
@@ -22,46 +24,62 @@ def recover(city):
     }
 
     url = "http://indicadoressifilis.aids.gov.br/tabelas.php"
-    response = requests.post(url, headers=headers, data=data)
+    response = requests.get(url, headers=headers, data=data)
 
     html_data = response.text
-    print(type(html_data))
+    # print(html_data)
 
     # Create a BeautifulSoup object from the HTML: soup
     soup = BeautifulSoup(html_data, features="html.parser")
 
-    print('Searching for table 10.A')
-
-    # Search for Tabela 10.A - Casos de sífilis congênita segundo diagnóstico
-    # final por ano de diagnóstico. Brasil, 1998-2018.
     tables = soup.findAll("table")
     for table in tables:
         table_label = table.previous_element
-        if "Tabela 10.A" in table_label:
+        if "Tabela 12.A" in table_label:
             # Find all columns that describe the years
             tag = "headertab"
             columns_year = [c.text for c in table.find(id=tag).findAll('th')]
             columns_year.pop(0)
 
             df = pd.read_html(str(table))[0]
+            print(df)
             df['ibge'] = city
 
             final_df = df.pivot(index='ibge', columns='Diagnóstico Final',
                                 values=columns_year)
 
-            return final_df
+            final_df.to_csv('results/{}.csv'.format(city), sep=';')
 
-    return None
+    return True
 
 
 def recover_all():
     """ Recover all cities
     """
 
-    codigos = pd.read_csv("data/codigo_municipio.csv", header=None)[0].values
+    codes = pd.read_csv("data/codigo_municipio.csv", header=None)[0].values
     df = pd.DataFrame()
 
-    for codigo in codigos:
-        df = df.append(recover(codigo))
+    # Create chunks to parallelize search
+    # chunks = create_chunks(codes, num_rows=1)
+    # pool = mp.Pool(4)
 
-    df.to_csv('data/final.csv')
+    # results = [pool.apply_async(recover, args=(x,)) for x in chunks]
+    # print([result.get() for result in results])
+    for city in codes[:5]:
+        recover(city)
+
+    # df.to_csv('results/final.csv')
+
+
+def create_chunks(dataframe, num_rows=1000):
+ return [dataframe[i:i+num_rows] for i in range(0,len(dataframe),num_rows)]
+
+# def execute():
+#  cities = City.objects.all().values('ibge')
+#  chunks = create_chunks(cities, num_rows=700)
+#  pool = mp.Pool(len(chunks))
+
+#  results = [pool.apply_async(recover, args=(x,)) for x in chunks]
+#  print([result.get() for result in results])
+
