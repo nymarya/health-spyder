ad7acc72c3d40d4aa360fdf0a758bcb57fca84cb
Mayra Azevedo <mayradazevedo@gmail.com>
2020-02-11 16:40:28 -0300

    clean code

--- src/scrapper.py
+++ src/scrapper.py
@@ -44,15 +44,39 @@ def recover(code, filename=None):
         columns_year.pop(0)
 
         # Save table to dataframe
-        df = pd.read_html(str(table))[0]
-        df['Table'] = ''.join(table_label.split('.')[0:2])
-        df.rename(columns={df.columns[0]: "Value"}, inplace=True)
-        final_df = final_df.append(df, ignore_index=True)
+        df = pd.read_html(str(table), decimal=',', thousands='.',
+                          na_values=['-'], keep_default_na=False)[0]
+
+        # Get columns referring to years
+        cols = df.columns[df.columns.str.startswith('20')]
+        cols2 = df.columns[df.columns.str.startswith('19')]
+        columns = list(set(cols).union(set(cols2)))
+        
+        # Melt dataframe to group all years to one column 
+        melted = pd.melt(df, id_vars=[df.columns[0]], value_vars=columns,
+                         var_name='Ano', value_name='Valor')
+        melted['Tabela'] = ''.join(table_label.split('.')[0:2])
+        melted.rename(columns={melted.columns[0]: "Indicador"}, inplace=True)
+
+        # Append table data
+        final_df = final_df.append(melted, ignore_index=True)
 
     if filename is None:
         filename = code
+
     print("Saving {}.csv".format(filename))
-    final_df.to_csv('results/{}.csv'.format(filename), sep=';')
+
+    # Recover numeric columns
+    cols = final_df.columns[final_df.columns.str.startswith('20')]
+    cols2 = final_df.columns[final_df.columns.str.startswith('19')]
+
+    # Tranform string to numeric
+    final_df[cols] = final_df[cols].apply(pd.to_numeric)
+    final_df[cols2] = final_df[cols2].apply(pd.to_numeric)
+
+    # Save CSV
+    final_df.to_csv('results/{}.csv'.format(filename), sep=';',
+                    decimal=',')
 
     return True
 
@@ -64,27 +88,23 @@ def recover_cities():
     codes = pd.read_csv("data/codigo_municipio.csv", header=None)[0].values
     df = pd.DataFrame()
 
-    # Get codes that were not downloaded yet
-    files = [int(f.split('.')[0]) for f in listdir('results/')
-             if isfile(join('results/', f))]
-    codes = list(set(codes) - set(files))
-
-    # Create chunks to parallelize search
-    # chunks = create_chunks(codes, num_rows=1)
+    # Parallelize downloads
     pool = mp.Pool(4)
 
     results = pool.map(recover, codes)
 
 
 def recover_regions():
+    """ Recover data from regions, states and country.
+    """
     pairs = pd.read_csv("data/codigos.csv", header=None, index_col=None).values
 
     codes = [(pair[0], pair[1]) for pair in pairs]
-
     pool = mp.Pool(4)
 
     results = pool.starmap(recover, codes)
 
 
 def recover_all():
+    recover_cities()
     recover_regions()
